613fb5de8816f92d50de986f3dcf6b4702c69a75
sarwar1ahammed <110556391+sarwar1ahammed@users.noreply.github.com>
2022-09-17 07:21:09 -1200

    Create Ques07.c

---
+++ Ques07.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+    // Print all Prime numbers between two given numbers.
+
+    int num1, num2, divider, flag;
+    printf("Enter two numbers as range of Prime Number. : ");
+    scanf("%d %d", &num1, &num2);
+
+    printf("Prime Numbers between %d to %d are : ", num1, num2);
+    for (num1=num1; num1<=num2; num1++)
+    {
+        flag = 0;
+        for (divider=2; divider<=(num1/2); divider++)
+        {
+            if (num1 % divider == 0)
+                flag++;
+        }
+
+        if (flag == 0)
+            printf("%d ", num1);
+    }
+
+
+    printf("\n");
+
+    return 0;
+}
